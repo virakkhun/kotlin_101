f459862723e453b0adf2e40386be82805d415431
virakkhun <rakmanoban@gmail.com>
2022-12-06 21:11:28 +0700

    variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,33 @@
 fun main(args: Array<String>) {
+    // @ variables with `val`
+    // read-only variables which can be assigned only once
+    // Un-annotate variable since it is inferred `Int`
+    val sumOfNormalFunction = sum(10, 10)
+
+    // Annotate type variable
+    val sumOfSortFunction: Int = sumInfer(30, 20)
+
+    // Variable without initializer has to be explicit type annot
+    val requiredWhenNoInitializer: String
+    // assign value to val above
+    requiredWhenNoInitializer = "Hi"
+
+    // @ variables with `var`
+    // re-assignable
+    var name: String = "Kotlin"
+    var age: Int = 20
+
+    // print with a new link break
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
+
+    println("==========================================")
+
+    println(sumOfNormalFunction)
+    println(sumOfSortFunction)
+    println(requiredWhenNoInitializer)
+
+    println("==========================================")
+
+    println("My name is $name and I am $age years old!")
 }
